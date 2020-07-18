5b10033fd8b1721911939be1834079ced2b5960c
Tushar Saxena <stushar345@gmail.com>
2020-07-19 02:29:25 +0530

    Update dsl.groovy

--- dsl.groovy
+++ dsl.groovy
@@ -1,89 +1,75 @@
-  
 job("devops_task6_job1"){
-  description("Pull the data from github repo automatically when some developers push code to github")
-  scm{
-    github("stushar12/task6_devops","master")
-  }
-  triggers {
-    scm("* * * * *")
-  }
-  steps{
-    shell('''if ls / | grep task6_devops
+    description("Download Data From Github And Create Docker Image")
+	scm{
+		github('stushar12/task6_devops' , 'master')
+	}
+	triggers{
+		scm(" * * * * * ")
+	}
+	steps{
+		shell('''if sudo ls -d /devops_task6
 then
-sudo rm -rf /task6_devops
+sudo rm -rvf /devops_task6
+sudo mkdir /devops_task6
+sudo cp -vrf * /devops_task6
 else
-sudo mkdir /task6_devops
-sudo cp -rvf * /task6_devops
+sudo mkdir /devops_task6
+sudo cp -vrf * /devops_task6
 fi
+cd /devops_task6/
+sudo docker build -t my-apache2:v5 .
+sudo docker tag  my-apache2:v5  disha1822/my-apache2:v5
+sudo docker push disha1822/my-apache2:v5
 ''')
-  }
+	}
+
 }
 
-job("devops_task6_job2"){
-  description("By looking at the code it will launch the deployment of respective webserver and the deployment will launch webserver, create PVC and expose the deployment")
-
-  triggers {
-    upstream("devops_task6_job1", "SUCCESS")
-  }
-  steps{
-    shell('''data=$(sudo ls /task6_devops)
-if  sudo ls /task6_devops/ | grep '[a-z].php'
+job("task6_job2"){
+    description("Launching & Monitoring Pods")
+	triggers{
+		upstream('task6_job1' , 'SUCCESS')
+	}
+	steps{
+		shell('''if sudo kubectl get deployment webapp
 then
-echo "It is a php code"
-
-if sudo kubectl get deploy | grep webserver
-then
-echo "php container is running"
+echo "Deployment already exists"
 else
-sudo kubectl create -f /task6_devops/deploy.yml
-fi
-else
-echo "No php code found"
+sudo kubectl create -f /devops_task3/web-deploy.yml
 fi
 ''')
-  }
+	}
+
 }
 
-
-job("devops_task6_job3"){
-  description("Check whether the web app is working or not. If it is not working sent email to developer")
-  triggers {
-    upstream("devops_task6_job2", "SUCCESS")
-  }
-  steps{
-    shell('''if sudo ls /task6_devops | grep html
+job("task6_job3"){
+    description("Testing Application")
+	triggers{
+		upstream('task6_job2' , 'SUCCESS')
+	}
+	steps{
+		shell('''status=$(curl -o /dev/null  -s  -w "%{http_code}"  192.168.99.108:30000)
+if [ $status == 200 ]
 then
-status=$(sudo curl -o /dev/null -s -w "%{http_code}" 192.168.99.101:30100)
-elif sudo ls /task6_devops | grep php
-then
-status=$(sudo curl -o /dev/null -s -w "%{http_code}" 192.168.99.101:30100)
-fi
-if [[ status -ne 200 ]]
-then 
-sudo python3 mail.py
+exit 1
 else
-echo "Code is fine"
-fi
-''')
-  }
-}
-
-job("devops_task6_job4"){
-  description("For monitoring of the container and to launch another if the existing fails.")
-
-  triggers {
-    upstream("devops_task6_job3", "SUCCESS")
-  }
-shell('''if sudo kubectl get deployment | grep webserver
-then
 exit 0
-else
-sudo kubectl create -f /task6_devops/deploy.yml
-sleep 10
 fi
 ''')
+	}
+
 }
 
+job("task6_job4"){
+    description("Sending Notification If App Fail")
+	triggers{
+		upstream('task6_job3' , 'SUCCESS')
+	}
+	steps{
+		shell("python3 /mail/mail.py")
+	}
+
+}
 
 
 buildPipelineView("Pipeline view") {
